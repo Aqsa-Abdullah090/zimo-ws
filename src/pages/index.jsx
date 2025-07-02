5127c644c476e68c7012d7823d1a7e263e06ce2d
Aqsa-Abdullah090 <aqsa.abdull@zimogroup.org>
2025-07-02 09:00:06 +0500

    create index file

---
+++ src/pages/index.jsx
@@ -0,0 +1,32 @@
+import Head from "next/head";
+import HoldingPage from "@/components/holding/holdingpage";
+import META_TAGS from "../lib/meta_tags";
+
+export default function Page() {
+  const { title, author, desc, image } = META_TAGS.main;
+
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="author" content={author} />
+        <meta name="description" content={desc} />
+
+        {/* Open Graph (Facebook) */}
+        <meta property="og:title" content={title} />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content={image} />
+
+        {/* Twitter Cards */}
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={desc} />
+        <meta name="twitter:image" content={image} />
+        <meta name="twitter:card" content="summary_large_image" />
+
+        {/* Favicon */}
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+      </Head>
+      <HoldingPage />
+    </>
+  );
+}
